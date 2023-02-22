9b49dcc2f3db4619b0577cdcfb29d7ce0f113fc6
方啸云 <fangxiaoyun@yuctime.com>
2023-02-22 19:54:43 +0800

    项目初始化

---
+++ app/src/main/java/com/ycfangxiaoyun/gitrebase/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ycfangxiaoyun.gitrebase
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val a = 1
+    }
+}
